4ea0c4c25db52b0a14dae93a073496e24eda4973
gaberoonie <gabe@tourbrat.com>
2023-09-15 17:43:11 +0900

    front end updates

--- README.md
+++
@@ -1,9 +0,0 @@
-# prjx-swap
-
-npm run build
-
-npm install -g serve
-or
-yarn global add serve
-
-serve -s build
--- static/js/4.585bdce3.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpdestiny-war"]=this["webpackJsonpdestiny-war"]||[]).push([[4],{431:function(s,n){}}]);
-//# sourceMappingURL=4.585bdce3.chunk.js.map
